b7c787947151ccdf1d2ac42c05bcabbf201703b1
Xu Yiming <xuyiming.open@outlook.com>
2023-03-22 16:20:18 +0800

    feat: Save zip

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+import JSZip from "jszip";
+
 import read from "./read.js";
+import download from "./download.js";
 
 /** @param {FileList} files */
 export default async function main(files) {
 	const [file] = files;
+	const zip = new JSZip();
 
 	for await (let entry of read(file)) {
-		console.log(entry);
+		console.debug(entry);
+		zip.file(entry.name.replace(/^\//, ""), entry.body);
 	}
+
+	const zipFilename = (/\.[^.]+$/).test(file.name) ? file.name.replace(/\.[^.]+$/, ".zip") : `${file.name}.zip`;
+	const zipBlob = await zip.generateAsync({ type: "blob" })
+	const zipUrl = URL.createObjectURL(zipBlob)
+	download(zipUrl, zipFilename);
+	URL.revokeObjectURL(zipUrl)
 }
